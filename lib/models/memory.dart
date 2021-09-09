2a665bbe2c3559ce123a219a001cc425c690f45e
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2021-09-09 18:37:16 -0300

    feat: final logic

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,13 +1,19 @@
 class Memory {
-  static const operations = const ['%', '±', '/', 'x', '-', '+', '='];
+  static const operations = const ['%', '/', 'x', '-', '+', '='];
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  String? _operation;
   String _value = '0';
+  String? _operation;
+  String? _lastCommand;
   bool _wipeValue = false;
 
   void applyCommand(String command) {
+    if (_isRaplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
+
     if (command == 'AC') {
       _allClear();
     } else if (operations.contains(command)) {
@@ -15,19 +21,35 @@ class Memory {
     } else {
       _addDigit(command);
     }
+
+    _lastCommand = command;
+  }
+
+  _isRaplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        operations.contains(command) &&
+        _lastCommand != '=' &&
+        command != '=';
   }
 
   _setOperation(String newOperation) {
+    bool isEqualSign = newOperation == '=';
     if (_bufferIndex == 0) {
-      _operation = newOperation;
-      _bufferIndex = 1;
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
     } else {
       _buffer[0] = _calculate();
       _buffer[1] = 0.0;
       _value = _buffer[0].toString();
       _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
     }
-    _wipeValue = true;
+    _wipeValue = true; // !isEqualSign;
   }
 
   _addDigit(String digit) {
@@ -48,6 +70,10 @@ class Memory {
 
   _allClear() {
     _value = "0";
+    _buffer.setAll(0, [0.0, 0.0]);
+    _bufferIndex = 0;
+    _operation = null;
+    _wipeValue = false;
   }
 
   _calculate() {
@@ -61,8 +87,6 @@ class Memory {
       case '-':
         return _buffer[0] - _buffer[1];
       case 'x':
-        return _buffer[0] * _buffer[1];
-      case '±':
         return _buffer[0] + _buffer[1];
       default:
         return _buffer[0];
