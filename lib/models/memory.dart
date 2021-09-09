ebb303b3371c0fb7b3d9f5768bc994fcc4b0574c
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2021-09-09 17:54:22 -0300

    feat: building structure for math

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,16 +1,72 @@
 class Memory {
-  String _value = "0";
+  static const operations = const ['%', '±', '/', 'x', '-', '+', '='];
+
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  String? _operation;
+  String _value = '0';
+  bool _wipeValue = false;
 
   void applyCommand(String command) {
     if (command == 'AC') {
       _allClear();
+    } else if (operations.contains(command)) {
+      _setOperation(command);
     } else {
-      _value += command;
+      _addDigit(command);
     }
   }
 
+  _setOperation(String newOperation) {
+    if (_bufferIndex == 0) {
+      _operation = newOperation;
+      _bufferIndex = 1;
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+    }
+    _wipeValue = true;
+  }
+
+  _addDigit(String digit) {
+    final isDot = digit == '.';
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+    if (isDot && _value.contains('.') && !wipeValue) {
+      return;
+    }
+
+    final emptyValue = isDot ? '0' : '';
+    final currentValue = wipeValue ? emptyValue : _value;
+    _value = currentValue + digit;
+    _wipeValue = false;
+
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
+  }
+
   _allClear() {
-    _value = "";
+    _value = "0";
+  }
+
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '±':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
+    }
   }
 
   String get velue {
